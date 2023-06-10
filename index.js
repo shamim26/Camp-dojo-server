8be769ac83b878f23dcc159dac8a458fe0e5e4c1
shamim26 <shamimreja101@gmail.com>
2023-06-10 18:50:16 +0600

    deployed

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // collections
     const classCollection = client.db("campDB").collection("classes");
